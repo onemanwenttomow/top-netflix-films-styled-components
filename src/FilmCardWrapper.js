40eab3f43daaea6083fe9d9209be500af81f8f75
Peter Anderson <pete.anderson@gmail.com>
2021-04-02 21:51:59 +0200

    test to see if this fixes mobile

--- src/FilmCardWrapper.js
+++ src/FilmCardWrapper.js
@@ -3,23 +3,13 @@ import styled from "styled-components/macro";
 import FilmCard from "./FilmCard";
 
 function FilmCardWrapper({ films, heading }) {
-    // show first 10 films. when user scrolls to end, then add more films using
-    // intersectionobserver api
     const [numberToShow, setNumberToShow] = useState(10);
     const scrollContainer = useRef();
     useEffect(() => {
-        // console.log("use effect in film card wrapper", scrollContainer);
-        // console.log("scrollContainer.current.offsetWidth", scrollContainer.current.offsetWidth);
-        // console.log("scrollContainer.current.scrollWidth", scrollContainer.current.scrollWidth);
-        // console.log("scrollContainer.current.scrollLeft", scrollContainer.current.scrollLeft);
-        // console.log(
-        //     "last child",
-        //     scrollContainer.current.children[numberToShow -1]
-        // );
         let options = {
             root: scrollContainer.current,
             rootMargin: "0px",
-            threshold: 1.0
+            threshold: 0.9
         };
         const filmToWatch = scrollContainer.current.children[numberToShow - 1];
 
@@ -30,8 +20,6 @@ function FilmCardWrapper({ films, heading }) {
         let callback = (entries, observer) => {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
-                    console.log("entry", entry);
-                    console.log("bring in more films.");
                     console.log("remove observer from this element", filmToWatch);
                     observer.unobserve(filmToWatch);
                     setNumberToShow((num) => (num += 10));
@@ -43,13 +31,12 @@ function FilmCardWrapper({ films, heading }) {
 
         return () => console.log("cleaning up");
     }, [numberToShow]);
-    console.log('numberToShow: ',numberToShow);
     films = films.slice(0, numberToShow);
     return (
         <>
             <h3>{heading}</h3>
             <FilmsContainer ref={scrollContainer}>
-                {films.map((film, idx) => (
+                {films.map((film) => (
                     <FilmCard film={film} key={film.netflixid} />
                 ))}
             </FilmsContainer>
