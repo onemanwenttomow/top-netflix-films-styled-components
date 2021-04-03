fc760081744c009fd9fc011fa554d15e966022e9
Peter Anderson <pete.anderson@gmail.com>
2021-04-03 15:56:28 +0200

    test to see if this fixes random dupes

--- functions/netflix-films.js
+++ functions/netflix-films.js
@@ -2,6 +2,29 @@ const axios = require("axios");
 const RAPIDAPIKEY = process.env.RAPIDAPIKEY;
 
 exports.handler = async (event, context) => {
+    let films = [];
+    var options = {
+        headers: {
+            "X-RapidAPI-Host": "unogs-unogs-v1.p.rapidapi.com",
+            "X-RapidAPI-Key": RAPIDAPIKEY
+        }
+    };
+    const imdbVotes = "gt100000";
+    const startYear = "1980";
+    const endYear = "2021";
+    const minImdbRating = "7";
+    let p = 1;
+
+    async function getAllFilms() {
+        let url = `https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew4000-!${startYear},${endYear}-!0,5-!${minImdbRating},10-!0-!Movie-!Any-!Any-!${imdbVotes}&t=ns&cl=39&st=adv&ob=Rating&p=${p}&sa=and`;
+        const { data } = await axios.get(url, options);
+        films = [...films, ...data.ITEMS];
+        if (data.COUNT > films.length) {
+            ++p;
+            return getAllFilms();
+        }
+        return films;
+    }
     try {
         const films = await getAllFilms();
         return {
@@ -13,26 +36,4 @@ exports.handler = async (event, context) => {
     }
 };
 
-let films = [];
-var options = {
-    headers: {
-        "X-RapidAPI-Host": "unogs-unogs-v1.p.rapidapi.com",
-        "X-RapidAPI-Key": RAPIDAPIKEY
-    }
-};
-const imdbVotes = "gt100000";
-const startYear = "1980";
-const endYear = "2021";
-const minImdbRating = "7";
-let p = 1;
 
-async function getAllFilms() {
-    let url = `https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew4000-!${startYear},${endYear}-!0,5-!${minImdbRating},10-!0-!Movie-!Any-!Any-!${imdbVotes}&t=ns&cl=39&st=adv&ob=Rating&p=${p}&sa=and`;
-    const { data } = await axios.get(url, options);
-    films = [...films, ...data.ITEMS];
-    if (data.COUNT > films.length) {
-        ++p;
-        return getAllFilms();
-    }
-    return films;
-}
